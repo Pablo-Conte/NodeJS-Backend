49ffeffd78275688852ec7b05103b9631f22f97e
PabloConte <pablo.correa.nr@gmail.com>
2022-07-08 20:25:28 -0300

    Controller e Rotas sendo estudadas 👀

--- src/app.mjs
+++ src/app.mjs
@@ -1,25 +1,20 @@
 import express, {Router} from "express";
 import cors from "cors";
+import routes from './routes.mjs'
 
 class App {
-    constructor(){
-        this.server = express()
-        this.middlewares();
-        this.routes();
+    constructor(){ //Estamos aqui setando o construtor da classe app
+        this.server = express() //que vai chamar seus próprios comportamentos
+        this.middlewares();      
+        this.routes();           
     }
 
-    middlewares(){
-        this.server.use(express.json());
-        this.server.use(cors())
+    middlewares(){//Os middlewares são funções a serem executadas até chegar chegar em nossas rotas
+        this.server.use(express.json()); //setando o que nosso server vai receber
+        this.server.use(cors()) //pré configurações de origens de requisições
     }
 
     routes(){
-        const routes = new Router();
-
-        routes.get("/hello", (req, res) => {
-            res.json({hello: "World"})
-        });
-
         this.server.use(routes)
     }
 }
